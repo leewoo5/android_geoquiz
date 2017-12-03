c88c3b2adf931212a13c943ebf3d5e4230305195
unknown <leewoo5629@gmail.com>
2017-12-03 17:04:33 +0900

    This is Start line Version

---
+++ app/src/main/java/com/topfactory/geoquiz/QuizActivity.java
@@ -0,0 +1,44 @@
+package com.topfactory.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private TextView mQuizeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.button_true);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,
+                                R.string.correct_toast,
+                                Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.button_false);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,
+                                R.string.incorrect_toast,
+                                Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mQuizeView = (TextView) findViewById(R.id.quiz_view);
+
+    }
+}
